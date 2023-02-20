23a67bbe603b950188f1f578839b200798de1db6
Matthew <mtaylorex@gmail.com>
2023-02-20 12:39:42 -0500

    Add debug lines

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -6,10 +6,11 @@ async function connect() {
   const dbUri = config.get<string>("dbUri");
 
   try {
+    console.log(`attempting to connect to db at ${dbUri}...`);
     await mongoose.connect(dbUri);
     console.log("DB connected");
   } catch (error) {
-    console.log("Could not connect to db");
+    console.log("Could not connect to db", error.message);
     process.exit(1);
   }
 }
